c0ec7c529068383465f739585a21b4a6bba23263
Andreas Ek <andreas@andreasek.se>
2015-11-13 20:18:30 +0100

    Title changed

--- web/app/themes/nordic-iron/index.php
+++ web/app/themes/nordic-iron/index.php
@@ -6,7 +6,7 @@
 	<link rel="icon" type="image/png" sizes="96x96" href="<?=get_stylesheet_directory_uri()?>/assets/img/favicon.png">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	
-	<title>Get Shit Done Kit Pro Tutorial by Creative Tim</title>
+	<title>Nordic Iron</title>
 
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
