b36db0638c46f09db6b9c5beca21d0252d8b467a
Andreas Ek <andreas@andreasek.se>
2015-11-13 20:17:44 +0100

    GA added

--- web/app/themes/nordic-iron/index.php
+++ web/app/themes/nordic-iron/index.php
@@ -24,6 +24,17 @@
 
 <body>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60162402-4', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <nav class="navbar navbar-default navbar-transparent navbar-fixed-top" role="navigation">
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
