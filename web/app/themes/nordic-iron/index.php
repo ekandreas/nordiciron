f7b22b2515db047bf87f6e9b1c9ac3103786b975
Andreas Ek <andreas@andreasek.se>
2015-11-13 18:25:09 +0100

    deploy added

---
+++ web/app/themes/nordic-iron/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Nordic Iron</title>
+	</head>
+	<body>
+		<p>&nbsp;</p>
+		<div style="text-align:center">
+			<img src="<?=get_stylesheet_directory_uri(); ?>/nordic-iron.png" alt="nordic-iron logo" />
+		</div>
+	</body>
+</html>
