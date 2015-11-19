57a9c930e18d6cf8627e502323df50cc0e960f84
Andreas Ek <andreas@andreasek.se>
2015-11-19 19:38:22 +0100

    Public categories

--- web/app/themes/nordic-iron/lib/page-types/options.php
+++ web/app/themes/nordic-iron/lib/page-types/options.php
@@ -12,6 +12,26 @@ class Main_Option_Type extends Papi_Option_Type {
 
     public function register() {
 
+        $this->box( 'Categories', [
+            papi_property( [
+                'slug'  => 'public_categories',
+                'title' => 'Public Categories',
+                'type'  => 'repeater',
+                'description' => 'Selected categories will show on pages as public reference links',
+                'settings' => [
+                'items' => [
+                  [
+                    'slug' => 'term',
+                    'type' => 'term',
+                    'settings' => [
+                      'taxonomy' => 'category'
+                    ]
+                  ]
+                ] 
+                ]
+            ] ),
+        ] );
+
         $this->box( 'Footer', [
             papi_property( [
                 'slug'  => 'footer_text',
--- web/app/themes/nordic-iron/templates/parts/all_cat_buttons.php
+++ web/app/themes/nordic-iron/templates/parts/all_cat_buttons.php
@@ -7,8 +7,20 @@
             <?php 
             $categories = get_categories();
             if( $categories ) {
+
+                $public_categories_field = papi_get_option( 'public_categories' );
+                $public_categories = [];
+
+                if( $public_categories_field ) {
+                    foreach ($public_categories_field as $key => $cat ) {
+                        $public_categories[] = $cat['term']->term_id;
+                    }                    
+                }
+
                 foreach ($categories as $key => $category) {
-                    echo '<a class="btn btn-default" href="/category/' . $category->slug . '">' . $category->name . '</a>&nbsp;';
+                    if( in_array( $category->term_id, $public_categories ) ) {
+                        echo '<a class="btn btn-default" href="/category/' . $category->slug . '">' . $category->name . '</a>&nbsp;';
+                    }
                 }
             }
             ?>
